f21669ebcc2aafb21f255f7f611a21089748ec50
tuan anh <hoangtuananh1997tb@gmail.com>
2018-07-18 08:23:22 +0700

    v3

---
+++ assets/js/myjquery.js
@@ -0,0 +1,6 @@
+ $(document).ready(function(){
+         	$('.thuonghieu li button').click(function(){
+         		$('.thuonghieu li button').removeClass('active');
+         		$(this).addClass('active');
+         	})
+         })
